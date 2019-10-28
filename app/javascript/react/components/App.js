97d94685ba0f9f311f23a73467d840103f602cf7
Daniel <hanakoscar@outlook.com>
2019-10-28 18:22:36 -0400

    styling and setup default colors

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from '../containers/Home'
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
+
+    get '/', to: "static_pages#index"
+
+  namespace :api do
+    namespace :v1 do
+      
+    end
+  end
 end
