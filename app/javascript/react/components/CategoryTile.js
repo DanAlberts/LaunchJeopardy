e10ff66231c27b4149437e762dd1868946b94a77
Daniel <hanakoscar@outlook.com>
2019-11-01 11:49:28 -0400

    Got basic modal fucntionality working

--- app/javascript/react/components/CategoryTile.js
+++ app/javascript/react/components/CategoryTile.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ClueTile from './ClueTile'
 
 const CategoryTile = (props) => {
-  const clueList = props.clues.map(question => {
+  const clueList = props.clues.map((question, index) => {
     return(
       <ClueTile
+        key={question.options.id}
+        id={index}
         clue={question.options.question}
         answer={question.options.answer}
         value={question.options.value}
@@ -12,8 +14,8 @@ const CategoryTile = (props) => {
     )
   })
   return(
-    < div className = "card-deck w-25" >
-      <div className="card bg-dark" >
+    <div className="card-deck w-25">
+      <div className="card bg-dark">
         <div className="card-body h-100">
           <h2 className="cTitle">{props.title}</h2>
           {clueList.slice(0,5)}
--- app/javascript/react/components/ClueTile.js
+++ app/javascript/react/components/ClueTile.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import Modali, { useModali } from 'modali'
 
 const ClueTile = (props) => {
+  let clue = props.clue
+  let answer = props.answer
+  const [completeExample, toggleCompleteModal] = useModali({
+    animated: true,
+    title: `${clue}`,
+    message: `${answer}`,
+    buttons: [
+      <Modali.Button
+        label="Cancel"
+        isStyleCancel
+        onClick={() => toggleCompleteModal()}
+      />
+    ],
+  });
+
   return(
-    <>
-  <div>
-    < div className = "card qCard bg-secondary" >
-      <div className="card-body">
-        <p>{props.value}</p>
-      </div>
-      <div class="card-footer">
+    <div>
+    <div>
+      <div className="card qCard bg-secondary">
+        <div className="card-body">
+          <p>{props.value}</p>
+        </div>
+        <div className="card-footer">
+          <button className="button-default" onClick={toggleCompleteModal}>Show Modal</button>
+        </div>
       </div>
     </div>
-  </div>
-    </>
+    <Modali.Modal {...completeExample} />
+    </div>
   )
 }
 
