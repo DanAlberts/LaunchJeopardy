932ae14edfbab19b784fb922927057e7fa270d95
Daniel <hanakoscar@outlook.com>
2019-11-07 15:57:42 -0500

    binding.pry left in code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,6 @@ class GamesController < ApplicationController
   def join_game
     created_game = GameCreator.new(current_user)
     created_game.generate_new_game
-binding.pry
     # redirect_to created_game.game
     redirect_to "/games/#{created_game.game.id}"
   end
